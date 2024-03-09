693cb15fe1cb2beb90cf3e25ff006f8be9e02bdf
mateuszprask <mateusz.prask@gmail.com>
2024-03-09 15:01:09 +0100

    update01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,19 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+let intervalId;
+
+document.querySelector('[data-start]').addEventListener('click', function () {
+  this.disabled = true;
+  document.querySelector('[data-stop]').disabled = false;
+  intervalId = setInterval(function () {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+document.querySelector('[data-stop]').addEventListener('click', function () {
+  document.querySelector('[data-start]').disabled = false;
+  this.disabled = true;
+  clearInterval(intervalId);
+});
